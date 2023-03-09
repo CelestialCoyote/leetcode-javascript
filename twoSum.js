f18c9f80ddf69dfe6424d34dc0bdffcbab1b5468
Paul Stearns <celestialcoyote@gmail.com>
2023-03-08 19:16:43 -0800

    Update twoSum

--- twoSum.js
+++ twoSum.js
@@ -12,27 +12,13 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-	for(let i = 0; i < nums.length; i++){
-        for(let j = i+1; j < nums.length; j++){
-            if(nums[i] + nums[j] == target){
-                return [i, j]
-            }
-        }
-    }
-	// let map = [];
-	// let indexnum = [];
-
-	// for (let i = 0; i < nums.length; i++) {
-	// 	if (map[nums[i]] != null) {
-	// 		index = map[nums[i]];
-	// 		indexnum[0] = index;
-	// 		indexnum[1] = i;
-	// 		break;
-	// 	} else {
-	// 		map[target - nums[i]] = i;
-	// 	};
-	// };
-	// return indexnum;
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[i] + nums[j] == target) {
+				return [i, j]
+			};
+		};
+	};
 };
 
 console.log(twoSum([2, 7, 11, 15], 9));
